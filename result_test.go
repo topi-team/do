e9ca737cfd489eaa995ac8a7da809a6a7570ec78
Ernesto Jiménez <ernesto@topi.eu>
2022-02-05 16:31:21 +0000

    fix and vet

--- result_test.go
+++ result_test.go
@@ -33,7 +33,7 @@ func ExampleWithReturn() {
 	// open missing_file: no such file or directory
 }
 
-func ExampleReturn() {
+func ExampleResult_Return() {
 	fileLines := func(file string) ([]string, error) {
 		f := do.WithReturn(os.Open(file))
 		limit := do.Map(f, func(r *os.File) io.Reader { return io.LimitReader(r, 10000) })
